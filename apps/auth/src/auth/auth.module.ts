4960104fe7ae72c0e2ae8ca3caa3a98ef8f484f9
emanuelvald <emanuel.vald@gmail.com>
2023-07-16 00:37:02 -0300

    auth

--- apps/auth/src/auth/auth.module.ts
+++ apps/auth/src/auth/auth.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ProxyQueues } from '@flight-reservations-api/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: ProxyQueues.USER,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            queue: ProxyQueues.USER,
+            urls: configService.get('AMQP_URL'),
+          },
+        });
+      },
+    },
+  ],
 })
 export class AuthModule {}
--- apps/auth/src/auth/auth.service.ts
+++ apps/auth/src/auth/auth.service.ts
@@ -1,14 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import {
   InactiveDto,
   LoginDto,
+  ProxyQueues,
   SignupDto,
+  UserMessagePattern,
 } from '@flight-reservations-api/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @Inject(ProxyQueues.USER) private readonly userService: ClientProxy,
+  ) {}
+
   async signup(signupDto: SignupDto): Promise<any> {
-    return 'Signup';
+    const obs$: Observable<any> = this.userService.send(
+      UserMessagePattern.CREATE,
+      signupDto,
+    );
+
+    return await firstValueFrom(obs$).catch((err) => {
+      throw new RpcException(err);
+    });
   }
 
   async login(loginDto: LoginDto): Promise<any> {
